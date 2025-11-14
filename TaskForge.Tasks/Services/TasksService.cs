2b03c3cdc7261a078c69ba18225e53a69b3e103b
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2025-11-14 21:23:29 +0200

    interface and class separation and file name change

---
+++ TaskForge.Tasks/Services/TasksService.cs
@@ -0,0 +1,45 @@
+﻿namespace TaskForge.Tasks.Services;
+
+public class TasksService : ITasksService
+{
+    public void CreateTask(string name, string description, DateTime dueDate)
+    {
+        var task = new TaskItem(name, description, dueDate);
+        // Logic to save the task to a database or in-memory collection
+    }
+
+    public void UpdateTaskStatus(Guid taskId, TaskStatus newStatus)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteTask(Guid taskId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TaskItem GetTask()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateSubtask(Guid parentTaskId, string name, string description)
+    {
+        var subtask = new Subtask(parentTaskId, name, description);
+        // Logic to save the subtask to a database or in-memory collection
+    }
+
+    public void UpdateSubtaskStatus(Guid subtaskId, TaskStatus newStatus)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteSubtask(Guid subtaskId)
+    {
+        throw new NotImplementedException();
+    }
+    public Subtask GetSubtask()
+    {
+        throw new NotImplementedException();
+    }
+}
